11912d51e7d36799d5fefcb69ce49e954e7b8f89
@dingyan <dy_100000@163.com>
2026-06-08 11:26:35 +0800

    增加opcua-webapi-typescript

---
+++ opcua-webapi-typescript/src/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './runtime';
+export * from './apis/index';
+export * from './models/index';
+export * from './opcua_attributes';
+export * from './opcua_constants';
+export * from './opcua_statuscodes';
